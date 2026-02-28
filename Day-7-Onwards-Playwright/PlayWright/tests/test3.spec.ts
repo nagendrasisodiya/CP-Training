7a3c007d97399c466b411a4d6918afb9b8f9aee7
nagendrasisodiya <3n3singh@gmail.com>
2026-02-28 10:50:16 +0530

    playwright restructred

--- Day-7-Onwards-Playwright/PlayWright/tests/test3.spec.ts
+++
@@ -1,7 +0,0 @@
-import {test} from "@playwright/test"
-
-test("tittle1", async({page})=>{
-    await page.goto("https://practicetestautomation.com/practice-test-login/")
-    await page.locator("input#password").fill("laptop")
-    await page.locator("button#submit").click()
-})
